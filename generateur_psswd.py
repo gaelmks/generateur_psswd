6de8b8ddad92aaabc2b941dffe2210f66a775957
gaelmks <gaelmukassa41@gmail.com>
2026-04-22 16:56:27 +0100

    premiere mise en forme de l'interface graphique

--- generateur_psswd.py
+++ generateur_psswd.py
@@ -1,6 +1,16 @@
+from tkinter import *
 import string
 import random
 
+window=Tk()
+window.title("password")
+window.minsize(480,360)
+window.maxsize(600, 600)
+window.config(background='#4065A4')
+
+#creation d;image 
 mixed= string.ascii_letters + string.digits
-psswd="".join(random.choice(caractere) for i in range(9))
+psswd="".join(random.choice(mixed) for i in range(9))
 print(f'mot de passe: {psswd}')
+
+window.mainloop()
